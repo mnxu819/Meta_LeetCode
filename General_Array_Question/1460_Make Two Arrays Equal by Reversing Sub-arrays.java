2720bbda296614068fef9706892381a21e32641e
mnxu819 <96892320+mnxu819@users.noreply.github.com>
2022-01-11 15:55:07 -0600

    Create 1460_Make Two Arrays Equal by Reversing Sub-arrays.java

---
+++ General_Array_Question/1460_Make Two Arrays Equal by Reversing Sub-arrays.java
@@ -0,0 +1,27 @@
+You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty sub-array of arr and reverse it. You are allowed to make any number of steps.
+Return true if you can make arr equal to target or false otherwise.
+
+Input: target = [1,2,3,4], arr = [2,4,1,3]
+Output: true
+Explanation: You can follow the next steps to convert arr to target:
+1- Reverse sub-array [2,4,1], arr becomes [1,4,2,3]
+2- Reverse sub-array [4,2], arr becomes [1,2,4,3]
+3- Reverse sub-array [4,3], arr becomes [1,2,3,4]
+There are multiple ways to convert arr to target, this is not the only way to do so.  
+  
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        if (target.length != arr.length) return false;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < target.length; i++) {
+            map.put(target[i], map.getOrDefault(target[i], 0)+1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0)-1);
+        }
+        for (int num : map.keySet()) {
+            if (map.get(num) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
