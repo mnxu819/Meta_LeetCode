944d52c039e0973d4a5de29d58adc8653f5f7dc0
mnxu819 <96892320+mnxu819@users.noreply.github.com>
2022-01-14 18:21:20 -0600

    Update and rename 219_Contains Duplicate II.java to 217_219_Contains Duplicate II.java

--- General_Array_Question/217_219_Contains Duplicate II.java
+++ General_Array_Question/217_219_Contains Duplicate II.java
@@ -18,3 +18,21 @@ class Solution {
         return false;
     }
 }
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+  
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
