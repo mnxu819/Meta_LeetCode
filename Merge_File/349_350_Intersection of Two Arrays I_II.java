969d13fcb2704547afef1d8717a2115fd4883f39
mnxu819 <96892320+mnxu819@users.noreply.github.com>
2022-01-14 18:16:52 -0600

    Update and rename 349_Intersection of Two Arrays.java to 349_350_Intersection of Two Arrays I_II.java

---
+++ Merge_File/349_350_Intersection of Two Arrays I_II.java
@@ -0,0 +1,56 @@
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                set2.add(num);
+            }
+        }
+        int[] res = new int[set2.size()];
+        int index = 0;
+        for (int set : set2) {
+            res[index++] = set;
+        }
+        return res;
+    }
+}
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0, k = 0;
+        //思路就是排序完，比较大小，谁大小的就进一位，直到相等，把nums1的k赋值为相等的，然后i j加一         
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
+
+
+
