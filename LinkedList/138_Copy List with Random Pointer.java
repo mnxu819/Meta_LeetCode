ddf9f41e08c9504a50bac56b2b5b5cd43c326c8c
mnxu819 <96892320+mnxu819@users.noreply.github.com>
2022-03-15 01:30:41 -0500

    Create 138_Copy List with Random Pointer.java

---
+++ LinkedList/138_Copy List with Random Pointer.java
@@ -0,0 +1,38 @@
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+Construct a deep copy of the list.
+  
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        //mirror old node to new node
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            Node newNode = new Node(cur.val);
+            map.put(cur, newNode);
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
